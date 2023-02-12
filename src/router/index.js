e1ea426c40b7b006e62721ce2d5b638ef5ebbf85
xiaozhiwen0322 <1809823474@qq.com>
2023-02-12 23:59:26 +0800

    完成路由导航

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import Home from "@/pages/Home"
+import Search from "@/pages/Search"
+import Login from "@/pages/Login"
+import Register from "@/pages/Register"
+export default new VueRouter({
+    routes: [
+        {
+            path: "/home",
+            component: Home,
+            meta: { show: true }
+        },
+        {
+            path: "/search",
+            component: Search,
+            meta: { show: true }
+        },
+        {
+            path: "/login",
+            component: Login,
+            meta: { show: false }
+        },
+        {
+            path: "/register",
+            component: Register,
+            meta: { show: false }
+        }
+    ]
+})
